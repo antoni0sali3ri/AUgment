93283d9b03530594b2305cdc61b3674c4865a69a
Antoni0 Sali3ri <contact@theopensourceguy.de>
2020-11-03 20:00:25 +0100

    Add module 'core-ui'

---
+++ core-ui/src/main/java/com/github/antoni0sali3ri/augment/ui/widget/numberpicker/NumberPickerExtensions.kt
@@ -0,0 +1,126 @@
+package com.github.antoni0sali3ri.augment.ui.widget.numberpicker
+
+import android.widget.NumberPicker
+import com.github.antoni0sali3ri.auc.IntTransform
+
+class NumberPickerExt(private val picker: NumberPicker) {
+
+    /**
+     * Delegate for [NumberPicker].value.
+     *
+     * @see NumberPicker.getValue
+     * @see NumberPicker.setValue
+     */
+    var value: Int
+        get() = picker.value
+        set(value) {
+            picker.value = value
+        }
+
+    /**
+     * Delegate for [NumberPicker].displayedValue (extension property).
+     */
+    var displayedValue: String
+        get() = picker.displayedValue
+        set(value) {
+            picker.displayedValue = value
+        }
+
+    /**
+     * Delegate for [NumberPicker].minValue.
+     *
+     * Automatically updates the displayed values according to [valueTransform].
+     *
+     * @see NumberPicker.getMinValue
+     * @see NumberPicker.setMinValue
+     * @see NumberPicker.setDisplayedValues
+     */
+    var minValue: Int
+        get() = picker.minValue
+        set(value) {
+            picker.updateValues(min = value, valueTransform = valueTransform)
+        }
+
+    /**
+     * Delegate for [NumberPicker].maxValue.
+     *
+     * Automatically updates the displayed values according to [valueTransform].
+     *
+     * @see NumberPicker.getMinValue
+     * @see NumberPicker.setMinValue
+     * @see NumberPicker.setDisplayedValues
+     */
+    var maxValue: Int
+        get() = picker.maxValue
+        set(value) {
+            picker.updateValues(max = value, valueTransform = valueTransform)
+        }
+
+    /**
+     * Function that defines how the [NumberPicker]'s displayedValues are generated.
+     *
+     * @see NumberPicker.setDisplayedValues
+     */
+    var valueTransform: IntTransform = identity
+        set(value) {
+            field = value
+            picker.updateValues(valueTransform = value)
+        }
+
+    companion object {
+        val identity: IntTransform = { it }
+    }
+}
+
+/**
+ * The current value as it is displayed.
+ */
+var NumberPicker.displayedValue: String
+    get() = displayedValues[value]
+    set(value) {
+        val v = displayedValues.indexOf(value)
+        require (v >= 0) {
+            "$value must be one of displayedValues!"
+        }
+        this.value = v
+    }
+
+/**
+ * Set up this [NumberPicker] to display the values of an enum class.
+ */
+inline fun <reified T: Enum<T>> NumberPicker.setUpWithEnum(valueTransform: (T) -> Any = { it }) {
+    val values = enumValues<T>()
+    minValue = 0
+    maxValue = values.size - 1
+    displayedValues = values.map { valueTransform(it).toString() }.toTypedArray()
+}
+
+/**
+ * Increment the current value by [delta].
+ */
+fun NumberPicker.increment(delta: Int = 1) {
+    require(delta > 0)
+    value += delta
+}
+
+/**
+ * Decrement the current value by [delta].
+ */
+fun NumberPicker.decrement(delta: Int = 1) {
+    require(delta > 0)
+    value -= delta
+}
+
+/**
+ * Update the values of this NumberPicker.
+ *
+ * @param min the minimum value. Defaults to [NumberPicker.getMinValue].
+ * @param max the maximum value. Defaults to [NumberPicker.getMaxValue].
+ * @param valueTransform the function to generate the displayed values. Defaults to the identity function.
+ */
+fun NumberPicker.updateValues(min: Int = minValue, max: Int = maxValue, valueTransform: IntTransform = {it}) {
+    displayedValues = null
+    minValue = min
+    maxValue = max
+    displayedValues = (min..max).map { valueTransform(it).toString() }.toTypedArray()
+}
---
+++ core-ui/src/main/java/com/github/antoni0sali3ri/augment/ui/widget/numberpicker/NumberPickers.kt
@@ -0,0 +1,149 @@
+package com.github.antoni0sali3ri.augment.ui.widget.numberpicker
+
+import android.widget.NumberPicker
+
+object NumberPickers {
+
+    /**
+     * Set up a set of NumberPickers as ordered.
+     *
+     * <p>Link the specified NumberPickers in such a way that</p>
+     * <ul>
+     *     <li>each NumberPicker has a range of <code>previousPicker.maxValue + 1</code> to
+     *         <code>nextPicker.minValue - 1</code></li>
+     *     <li>the first NumberPicker has a minimum value of minValue</li>
+     *     <li>the last NumberPicker has a maximum value of maxValue</li>
+     * </ul>
+     *
+     * <p><b>NOTE</b>: Making changes to the individual NumberPicker's minValue/maxValue properties can
+     * (and most likely will) result in undefined behaviour!</b></p>
+     *
+     * @param minValue the minimum value for the first NumberPicker
+     * @param maxValue the maximum value for the last NumberPicker
+     * @param pickers the NumberPickers to link to each other
+     * @throws IllegalArgumentException when the specified range of values is smaller than the number
+     *                                  of NumberPickers or when any NumberPicker is passed more than once
+     */
+    fun setUpAsOrdered(minValue: Int, maxValue: Int, vararg pickers: NumberPicker) {
+        val pickerCount = pickers.size
+        val rangeSize = maxValue - minValue + 1
+
+        require(pickerCount == pickers.toSet().size) {
+            "Do not pass a NumberPicker reference more than once to this function!"
+        }
+        require(rangeSize >= pickers.size) {
+            "Defined Range [$minValue..$maxValue] is too small to fit $pickerCount NumberPickers!"
+        }
+
+        pickers.first().minValue = minValue
+        pickers.last().maxValue = maxValue
+        pickers.forEachIndexed { index, picker ->
+            picker.setOnValueChangedListener { _, _, newVal ->
+                if (index > 0) {
+                    pickers[index - 1].maxValue = newVal - 1
+                }
+                if (index < pickerCount - 1) {
+                    pickers[index + 1].minValue = newVal + 1
+                }
+            }
+        }
+    }
+
+    /**
+     * Set up a set of NumberPickers as overflowing.
+     *
+     * <p>Link the specified NumberPickers in such a way that</p>
+     *
+     * <ul>
+     *     <li>when pickers[i] wraps around past its maxValue (overflow), increment value of pickers[i-1] by 1</li>
+     *     <li>when pickers[i] wraps around past its minValue (underflow), decrement value of pickers[i-1] by 1</li>
+     * </ul>
+     */
+    fun setUpAsOverflowing(pickers: List<NumberPicker>) {
+        val size = pickers.size
+        for (i in size - 1 downTo 1) {
+            pickers[i].setOnValueChangedListener { picker, oldValue, newValue ->
+                if (oldValue == picker.maxValue && newValue == 0) {
+                    pickers[i-1].increment()
+                }
+                if (newValue == picker.maxValue && oldValue == 0) {
+                    pickers[i-1].decrement()
+                }
+            }
+        }
+    }
+
+    fun setUpAsOverflowing(vararg pickers: NumberPicker) {
+        setUpAsOverflowing(pickers.toList())
+    }
+
+    fun setUpAsTimePicker(minutePicker: NumberPicker, secondPicker: NumberPicker) {
+        minutePicker.minValue = 0
+        minutePicker.maxValue = Int.MAX_VALUE
+        secondPicker.minValue = 0
+        secondPicker.maxValue = 59
+        setUpTimePicker(secondPicker, minutePicker)
+    }
+
+    fun setUpAsTimePicker(hourPicker: NumberPicker, minutePicker: NumberPicker, secondPicker: NumberPicker) {
+        hourPicker.minValue = 0
+        hourPicker.maxValue = Int.MAX_VALUE
+        minutePicker.minValue = 0
+        minutePicker.maxValue = 59
+        secondPicker.minValue = 0
+        secondPicker.maxValue = 59
+        setUpTimePicker(secondPicker, minutePicker, hourPicker)
+    }
+
+    fun setUpAsTimePicker(
+        dayPicker: NumberPicker,
+        hourPicker: NumberPicker,
+        minutePicker: NumberPicker,
+        secondPicker: NumberPicker
+    ) {
+        dayPicker.minValue = 0
+        dayPicker.maxValue = Int.MAX_VALUE
+        hourPicker.minValue = 0
+        hourPicker.maxValue = 24
+        minutePicker.minValue = 0
+        minutePicker.maxValue = 59
+        secondPicker.minValue = 0
+        secondPicker.maxValue = 59
+        setUpTimePicker(secondPicker, minutePicker, hourPicker, dayPicker)
+    }
+
+    fun setUpAsTimePicker(
+        weekPicker: NumberPicker,
+        dayPicker: NumberPicker,
+        hourPicker: NumberPicker,
+        minutePicker: NumberPicker,
+        secondPicker: NumberPicker
+    ) {
+        weekPicker.minValue = 0
+        weekPicker.maxValue = Int.MAX_VALUE
+        dayPicker.minValue = 0
+        dayPicker.maxValue = 6
+        hourPicker.minValue = 0
+        hourPicker.maxValue = 24
+        minutePicker.minValue = 0
+        minutePicker.maxValue = 59
+        secondPicker.minValue = 0
+        secondPicker.maxValue = 59
+        setUpTimePicker(secondPicker, minutePicker, hourPicker, dayPicker)
+    }
+
+    private fun setUpTimePicker(
+        secondPicker: NumberPicker,
+        minutePicker: NumberPicker,
+        hourPicker: NumberPicker? = null,
+        dayPicker: NumberPicker? = null,
+        weekPicker: NumberPicker? = null
+    ) {
+        val pickers = mutableListOf(minutePicker, secondPicker)
+        if (hourPicker != null) pickers.add(0, hourPicker)
+        if (dayPicker != null) pickers.add(0, dayPicker)
+        if (weekPicker != null) pickers.add(0, weekPicker)
+        setUpAsOverflowing(pickers)
+    }
+
+}
